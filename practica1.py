ac0533c595d0757d049cb534523e07edfa3ea8b8
Rachid Amar <43322@a.iesleopoldoqueipo.com>
2022-10-25 17:29:38 +0200

    Primera subida

---
+++ practica1.py
@@ -0,0 +1,7 @@
+age = int(input("Edad?"))
+if age <= 18:
+	print("Eres mayor de edad")
+elif age == 18
+
+
+# Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
---
+++ practica2.py
@@ -0,0 +1,6 @@
+key = "ymefumounpuro"
+password = input("Introduce tu contraseña")
+if key == password:
+	print ("correcto")
+else:
+	print("incorrecto")
